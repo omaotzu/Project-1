e443d40b1e4bcacd5d2f29f04e948a252159bafa
omaotzu <omarhphillips@gmail.com>
2017-02-10 15:10:08 +0000

    Clocks done - Multiple choice in progress

--- js/app.js
+++ js/app.js
@@ -1,15 +1,78 @@
 $(() => {
-  const $instructions = $('.instructions');
-  const $letsbegin = prompt('Please enter your name?');
+  // const $instructions = $('.instructions');
 
-  const $questions = $('questions');
+  const $questions = $('.questions');
+  const $name = $('.name');
+  const $roundtimer = $('.rounds');
+  const $questiontimer = $('.tunes');
+  const $button = $('button');
 
-  function startQuestions (){
-    $questions.text('firsttune');
-    $instructions.text('Quess the correct song as quickly as possible!');
-    console.log('DONE');
+  // const answers = ['a','b','c','d','e','f','g','h','i','j','k'];
+
+  const multipleanswers =
+    [{ audio: 'a',
+      options: ['b', 'a', 'c', 'g']},
+    { audio: 'b',
+      options: ['f', 'j','h','b']},
+    { audio: 'c',
+      options: ['i','b','c','g']}];
+
+
+
+  function qanda () {
+    for (let i = 0; i<multipleanswers.length; i++) {
+      
+    }
   }
 
-  $letsbegin.on('return', startQuestions);
+  const randomaudio = multipleanswers[Math.floor(Math.random()*multipleanswers.length)].audio;
+  const randomoptions = multipleanswers[Math.floor(Math.random()*multipleanswers.length)].options;
+
+  console.log(randomaudio);
+  console.log(randomoptions);
+
+
+
+  // function noDuplicates () {
+  //   if ($answers === $randomAnswer1) {
+  //     console.log($answers);
+  // //     console.log($answers);
+  //
+  //   }
+  // }
+  let roundTimeRemaining = 90;
+  let answerTimeRemaining =15;
+  let timerId = null;
+
+
+
+
+  function hideStuff() {
+    $name.hide();
+    $button.hide();
+  }
+  function startRoundTimer(){
+    timerId = setInterval(() => {
+      roundTimeRemaining--;
+      $roundtimer.text(roundTimeRemaining);
+      if (roundTimeRemaining <=0) {
+        clearInterval(timerId);
+      }
+    }, 1000);
+  }
+  function startQuestionTimer(){
+    timerId = setInterval(() => {
+      answerTimeRemaining--;
+      $questiontimer.text(answerTimeRemaining);
+      if (answerTimeRemaining <=0) {
+        clearInterval(timerId);
+      }
+    }, 1000);
+    startQuestionTimer();
+  }
+
+  $button.on('click', startRoundTimer);
+  $button.on('click', startQuestionTimer);
+  $button.on('click', hideStuff);
 
 });
